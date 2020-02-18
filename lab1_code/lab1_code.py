d265db352147d90836098b8bbf2de41ea18fe40d
Dylan Martinez <martinez.dylanj@gmail.com>
2020-02-17 22:17:32 -0500

    Fixed lab1 code and added output file

--- lab1_code/lab1_code.py
+++ lab1_code/lab1_code.py
@@ -8,7 +8,7 @@ KGB (Kewl Girls & Boys) | SPACE 584 | 02/11/2020 | High Altitude Balloon
 .VERSION CHANGES
 
 KGB (Kewl Girls & Boys) | SPACE 584 | 08/24/2018 | Created Project
-                                                 | 1) 
+                                                 | 1)
 .DESCRIPTION
 
 This will take in data from the KGB instruments and dynamically write to file.
@@ -27,7 +27,7 @@ import RPi.GPIO as GPIO
 import adafruit_am2320
 import adafruit_bno055
 import adafruit_bmp3xx
-import adafruit_mcp3xxx.mcp3008
+import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 
 ## initialize I2C bus
@@ -41,10 +41,10 @@ cs = digitalio.DigitalInOut(board.D22)
 am2320 = adafruit_am2320.AM2320(i2c)
 bno055 = adafruit_bno055.BNO055(i2c)
 bmp388 = adafruit_bmp3xx.BMP3XX_I2C(i2c)
-mcp3008 = adafruit_mcp3xxx.MCP3008(spi, cs)
+mcp3008 = MCP.MCP3008(spi, cs)
 
 ## analog input channel (thermistor)
-chan0 = AnalogIn(mcp3008, adafruit_mcp3xxx.P0)
+chan0 = AnalogIn(mcp3008, MCP.P0)
 
 ## conversion coefficients (thermistor)
 therm_a = 0.001468
@@ -70,11 +70,11 @@ def createCSV():
       "am2320_temperature",
       "bmp388_temperature",
       "bmp388_pressure",
-      "bno055_accelerometer",
-      "bno055_eulerAngle",
+      "bno055_acceleration",
+      "bno055_euler_angle",
       "bno055_gravity",
       "bno055_gyroscope",
-      "bno055_linearAcceleration",
+      "bno055_linear_acceleration",
       "bno055_magnetometer",
       "bno055_temperature",
       "bno055_quaternion",
@@ -96,8 +96,13 @@ def writeCSV(fileName):
     timestamp = datetime.timestamp(now)
 
     ## thermistor conversions
-    thermistor_r = therm_r2 * (therm_v_in/chan0.voltage - 1)
-    thermistor_temp = (1/(therm_a + therm_b * math.log(thermistor_r) + therm_c * pow(math.log(thermistor_r), 3))) - 273
+    if chan0.voltage != 0:
+        thermistor_r = therm_r2 * (therm_v_in/chan0.voltage - 1)
+        thermistor_temp = (1/(therm_a + therm_b * math.log(thermistor_r) + therm_c * pow(math.log(thermistor_r), 3))) - 273
+    else:
+        thermistor_r = None
+        thermistor_temp = None
+
 
     ## write line
     writer.writerow([
@@ -110,12 +115,12 @@ def writeCSV(fileName):
       bmp388.temperature,
       bmp388.pressure,
       ## bno055 (IMU)
-      bno055.accelerometer,
-      bno055.eulerAngle,
+      bno055.acceleration,
+      bno055.euler,
       bno055.gravity,
-      bno055.gyroscope,
-      bno055.linearAcceleration,
-      bno055.magnetometer,
+      bno055.gyro,
+      bno055.linear_acceleration,
+      bno055.magnetic,
       bno055.temperature,
       bno055.quaternion,
       ## mcp3008 (Thermistor)
