77ffbf89d35ca958cfb8e572a1bee4e3c7bdc975
Dylan Martinez <martinezdylan@anonymousmbp.local>
2020-02-25 15:10:34 -0500

    quick fix

--- adafruit_gps/ublox_gps_cosm.py
+++ adafruit_gps/ublox_gps_cosm.py
@@ -1,6 +1,7 @@
 import time
 import board
 import busio
+import csv
 
 import adafruit_gps
 
@@ -14,6 +15,19 @@ gps = adafruit_gps.GPS_GtopI2C(i2c, address=66, debug=False)
 gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
 
 gps.send_command(b'PMTK220,1000')
+
+## initialize file name
+fileName = 'lab2_output.csv'
+## open unique file
+with open(fileName, 'w', newline='') as file:
+    writer = csv.writer(file)
+
+    ## create header
+    writer.writerow([
+        "timestamp_EPOCHms",
+        "ublox_latitude",
+        "ublox_longitude",
+    ])
 
 last_print = time.monotonic()
 while True:
@@ -29,3 +43,14 @@ while True:
         print('=' * 40)  # Print a separator line.
         print('Latitude: {0:.6f} degrees'.format(gps.latitude))
         print('Longitude: {0:.6f} degrees'.format(gps.longitude))
+
+        with open(fileName, 'a', newline='') as file:
+            writer = csv.writer(file)
+
+            writer.writerow([
+            ## timestamp
+            timestamp,
+            ## coordinates (GPS)
+            '{0:.6f}'.format(gps.latitude),
+            '{0:.6f}'.format(gps.longitude)
+            ])
