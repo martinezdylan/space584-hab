4dcbc6acd7a6471d9d1192a1e6bdfced7790a047
Dylan Martinez <martinez.dylanj@gmail.com>
2020-02-25 14:30:20 -0500

    adding new gps code

---
+++ adafruit_gps/ublox_gps_cosm.py
@@ -0,0 +1,31 @@
+import time
+import board
+import busio
+
+import adafruit_gps
+
+SCL = board.SCL
+SDA = board.SDA
+
+i2c = busio.I2C(board.SCL, board.SDA)
+
+gps = adafruit_gps.GPS_GtopI2C(i2c, address=66, debug=False)
+
+gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+
+gps.send_command(b'PMTK220,1000')
+
+last_print = time.monotonic()
+while True:
+
+    gps.update()
+
+    current = time.monotonic()
+    if current - last_print >= 1.0:
+        last_print = current
+        if not gps.has_fix:
+            print('Waiting for fix...')
+            continue
+        print('=' * 40)  # Print a separator line.
+        print('Latitude: {0:.6f} degrees'.format(gps.latitude))
+        print('Longitude: {0:.6f} degrees'.format(gps.longitude))
